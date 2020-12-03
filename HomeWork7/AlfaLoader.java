29efa8f8a7509879c49e0b9420f4a6598d7bbf4f
Artem Sturchenko <geleldor@live.ru>
2020-12-03 17:01:37 +0300

    changes

--- HomeWork7/AlfaLoader.java
+++ HomeWork7/AlfaLoader.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -11,7 +12,8 @@ import java.util.regex.Pattern;
 /**
  * Загрузчик курса с сайта Альфа-Банка
  */
-public class AlfaLoader{
+public class AlfaLoader extends SiteLoader {
+    private Map<String, Double> map;
     private String line = null;
 
     private List<String> list = null;
@@ -39,58 +41,111 @@ public class AlfaLoader{
         return line;
     }
 
-    public double load(String word) {
-        double result;
-        line = urlToString();
-        list = toCollection(line);
-        deleteFromList(list);
-        result = currency(word);
-        return result;
-    }
-
     private static List<String> toCollection(String line) {
-        line = line.substring(line.indexOf("["),line.length()-1);
+        line = line.substring(line.indexOf("["), line.length() - 1);
         String[] array = line.split("},\\{");
         List<String> list = new LinkedList<>(Arrays.asList(array));
         return list;
     }
 
-    private static void deleteFromList(List<String> list) {
+    private static LinkedList<String> deleteFromList(List<String> list) {
+        LinkedList<String> newList = new LinkedList<>();
         Pattern pattern = Pattern.compile("(\"[A-Za-z]+\":\"[A-Z]+\")");
         Matcher matcher;
-        int position = 0;
-        for (int i = 0; i< list.size();i++) {
+        for (int i = 0; i < list.size(); i++) {
             matcher = pattern.matcher(list.get(i));
             while (matcher.find()) {
-                if(matcher.group().contains("\"buyCode\":933")) {
-                    if(!matcher.group().equals("\"buyCode\":933"))
-                        position = i;
+                if (matcher.group().contains("\"buyCode\"")) {
+                    if (matcher.group().equals("\"buyIso\":BYN"))
+                        newList.add(list.get(i));
                 }
             }
-            list.remove(position);
         }
+        return newList;
     }
+
     private double currency(String currency) {
         double result = 0;
         Pattern pattern = Pattern.compile("\"[A-Za-z]+\":\\d+");
         Matcher matcher;
-        for (int i = 0; i< list.size();i++) {
+        for (int i = 0; i < list.size(); i++) {
             matcher = pattern.matcher(list.get(i));
             while (matcher.find()) {
-                if(matcher.group().contains("\"sellCode\":"+currency)) {
+                if (matcher.group().contains("\"sellCode\":" + currency)) {
                     String newLine = list.get(i);
                     pattern = Pattern.compile("(\"[A-Za-z]+\":\\d+\\.\\d+)");
                     matcher = pattern.matcher(newLine);
                     while (matcher.find()) {
-                        if(matcher.group().contains("\"buyRate\":")) {
+                        if (matcher.group().contains("\"buyRate\":")) {
                             newLine = matcher.group();
-                            result = Double.parseDouble(newLine.substring(newLine.indexOf(":")+1));
+                            result = Double.parseDouble(newLine.substring(newLine.indexOf(":") + 1));
                         }
                     }
                 }
             }
         }
-    return result;
+        return result;
     }
 
+    @Override
+    public double load(Currency currencyName) {
+        map = new LinkedHashMap<>();
+        LocalDate today = LocalDate.now();
+        List<LocalDate> newlist = Collections.singletonList(today);
+        for (LocalDate localDate : newlist) {
+            try {
+                map.put(localDate + " | " + currencyName, handle(null, currencyName));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        for (Map.Entry<String, Double> entry : map.entrySet()) {
+            System.out.println("Date " + entry.getKey() + " | " + entry.getValue());
+        }
+
+        return 0;
+    }
+
+    @Override
+    protected double handle(String content, Currency currencyName) {
+        double result;
+        line = urlToString();
+        list = toCollection(line);
+        String curr = convertISOtoNBRB(currencyName);
+        result = currency(curr);
+        return result;
+    }
+
+    private static String convertISOtoNBRB(Currency currency) {
+        String line = null;
+        switch (currency.getId()) {
+            case "145": {
+                line = CurrencyСodes.getUSDCode();
+                break;
+            }
+            case "292": {
+                line = CurrencyСodes.getEURCode();
+                break;
+            }
+            case "298": {
+                line = CurrencyСodes.getRUBCode();
+                break;
+            }
+        }
+        return line;
+    }
+
+    public void toFile(SiteLoader.Currency[] currencies) {
+        for (int i = 0; i < currencies.length; i++) {
+            LocalDate today = LocalDate.now();
+            List<LocalDate> newlist = Collections.singletonList(today);
+            for (LocalDate localDate : newlist) {
+                try {
+                    ToFile.resultMap.put(localDate + " | " + currencies[i], handle(null, currencies[i]));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
--- HomeWork7/Test.java
+++ HomeWork7/Test.java
@@ -1,85 +1,58 @@
 package HomeWork7;
 
-import java.io.*;
+import static HomeWork7.SiteLoader.*;
+import static HomeWork7.SiteLoader.Currency.*;
 
 public class Test {
-    static String line = null;
-    static String writePath = null;
-    static String filename = File.separator+"currency.txt";
+    static boolean readyToWrite = true;
     public static void main(String[] args) {
         /*
           метод alfBankCurrency - Для вывода курсов с сайта банка Альфа-Банк
          */
-//        alfBankCurrency();
+        alfBankCurrency(new AlfaLoader());
+        if(readyToWrite) {
+            new ToFile().writeToFile(null, new AlfaLoader().toFile(new Currency[]{USD, EUR}));
+        }
         /*
           метод printRatesDates - Для вывода курсов с сайта Нац. Банка за период,укащзываемых в параметрах
          */
-        printRatesDates(new NBRBLoader(),args);
+//        printRatesDates(new NBRBLoader(),args);
+
+        /*
+          метод printRatesDates - Для вывода курсов с сайта Нац. Банка за текущую дату
+         */
 //        printRates(new NBRBLoader());
+
+
         /*
           Разовое сохранение в файл
          */
-//        saveToFileOnce("",new NBRBLoader());
+
         /*
           запись в файл с проверкой на наличие курсов ранее
          */
-//        readAndWriteCurrency();
-    }
-    public static void saveToFileOnce(String path, SiteLoader loader) {
-        double d1 = loader.load(SiteLoader.Currency.EUR);
-        double d2 = loader.load(SiteLoader.Currency.RUB);
-        double d3 = loader.load(SiteLoader.Currency.USD);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String line = null;
-        try {
-            line = reader.readLine();
-            path = line;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        if(!path.isBlank()) {
-            try {
-                PrintWriter writer = new PrintWriter(path+filename);
-                writer.println(SiteLoader.Currency.EUR+" is "+d1);
-                writer.println(SiteLoader.Currency.RUB+" is "+d2);
-                writer.println(SiteLoader.Currency.USD+" is "+d3);
-                writer.close();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-        }
-        else {
-            line = "c:\\Users\\sturchenko.a\\IdeaProjects\\HomeWork\\src\\HomeWork7\\";
-            try {
-                PrintWriter writer = new PrintWriter(line+filename);
-                writer.println(SiteLoader.Currency.EUR+" is "+d1);
-                writer.println(SiteLoader.Currency.RUB+" is "+d2);
-                writer.println(SiteLoader.Currency.USD+" is "+d3);
-                writer.close();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-        }
-        writePath = line;
+
     }
 
-    public static void printRates(SiteLoader loader){
-        /**
-         * Для вывода курсов в консоль
+    public static void printRates(NBRBLoader loader){
+        /*
+          Для вывода курсов в консоль
          */
-        System.out.println(loader.load(SiteLoader.Currency.EUR));
-        System.out.println(loader.load(SiteLoader.Currency.RUB));
-        System.out.println(loader.load(SiteLoader.Currency.USD));
+        loader.loadDates(USD);
+        loader.loadDates(EUR);
+        loader.loadDates(RUB);
 
     }
 
     public static void printRatesDates(NBRBLoader loader,String[] array) {
-        loader.loadDates(SiteLoader.Currency.EUR,array);
+        loader.loadDates(USD,array);
+        loader.loadDates(EUR,array);
+        loader.loadDates(RUB,array);
     }
-    public static void alfBankCurrency() {
-        System.out.println(new AlfaLoader().load(CurrencyСodes.getEUR()));
-        System.out.println(new AlfaLoader().load(CurrencyСodes.getRUB()));
-        System.out.println(new AlfaLoader().load(CurrencyСodes.getUSD()));
+    public static void alfBankCurrency(AlfaLoader loader) {
+        loader.load(USD);
+        loader.load(EUR);
+        loader.load(RUB);
     }
 
 }
