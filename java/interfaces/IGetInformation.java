105711c456bc35d5ff0a19fd0c432993605910a3
generethic <71727279+generethic@users.noreply.github.com>
2020-12-25 20:31:31 +0300

    Delete IGetInformation.java

--- java/interfaces/IGetInformation.java
+++
@@ -1,8 +0,0 @@
-package interfaces;
-
-import java.time.LocalDate;
-import java.util.LinkedHashMap;
-
-public interface IGetInformation {
-    LinkedHashMap<String,Double> getInformation(LocalDate...dates);
-}
